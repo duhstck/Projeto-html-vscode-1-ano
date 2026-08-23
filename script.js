5919f95473800b7419680581e5199e91cdc39634
duh <eduarda.cordeiro.silva26@escola.pr.gov.br>
2026-08-23 16:43:08 -0300

    alterações

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const botoes = document.querySelectorAll("button");
+// ================================
+// BOTÕES DE CURTIDA
+// ================================
+
+const botoes = document.querySelectorAll("article button");
 
 botoes.forEach(function (botao) {
+
     let curtiu = false;
 
     botao.addEventListener("click", botaoClicado);
 
     function botaoClicado() {
-        console.log("fui clicado");
 
         let texto = botao.querySelector("span");
 
@@ -20,16 +24,31 @@ botoes.forEach(function (botao) {
     }
 });
 
+
+// ================================
+// BOTÃO DO TEMA ESCURO
+// ================================
+
 const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
 
 btnTemaEscuro.addEventListener("click", mudaTema);
 
+
+// Função para alternar entre tema claro e escuro
 function mudaTema() {
+
     const corpoPagina = document.body;
 
     if (corpoPagina.classList.contains("tema-escuro")) {
+
         corpoPagina.classList.remove("tema-escuro");
+
+        btnTemaEscuro.textContent = "🌙";
+
     } else {
+
         corpoPagina.classList.add("tema-escuro");
+
+        btnTemaEscuro.textContent = "☀️";
     }
 }
